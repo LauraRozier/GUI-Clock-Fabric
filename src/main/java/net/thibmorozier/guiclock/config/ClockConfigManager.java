2674328cd7cbf51e49307e2e024d4b6457e85410
Thimo <thibmorozier@live.nl>
2021-09-15 19:17:16 +0200

    Remove more superfluous imports

--- src/main/java/net/thibmorozier/guiclock/config/ClockConfigManager.java
+++ src/main/java/net/thibmorozier/guiclock/config/ClockConfigManager.java
@@ -3,8 +3,6 @@ package net.thibmorozier.guiclock.config;
 import java.io.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
